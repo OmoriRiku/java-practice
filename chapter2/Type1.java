8bc934b43f86f7226fa44762dc99f4f571470bb4
OmoriRiku <oomori3321@gmail.com>
2023-05-18 09:10:03 +0000

    スッキリわかるJava入門 chapter2

---
+++ chapter2/Type1.java
@@ -0,0 +1,6 @@
+public class Type1 {
+  public static void main (String[] args) {
+    int age = (int)3.2;
+    System.out.println(age);
+  }
+}
---
+++ chapter2/Type2.java
@@ -0,0 +1,8 @@
+public class Type2 {
+  public static void main (String[] args) {
+    double d = 0.5 / 2;
+    long l = 5 + 2L;
+    System.out.println(d);
+    System.out.println(l);
+  }
+}
