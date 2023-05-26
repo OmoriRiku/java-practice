2c286dfb842bedfcd264769cf97a74c746e63c42
OmoriRiku <121606498+OmoriRiku@users.noreply.github.com>
2023-05-26 11:19:41 +0900

    スッキリわかるJava入門 chapter6

---
+++ chapter6/main/Kohan.java
@@ -0,0 +1,14 @@
+package comment;
+
+public class Kohan {
+  public static void callDeas() {
+    System.out.println("えぇい、こしゃくな。くせ者だ！であえい！");
+  }
+
+  public static void showMondokoro() throws Exception {
+    System.out.println("飛車さん、角さん。もういいでしょう。");
+    System.out.println("この紋所が目にはいらぬか！");
+    Thread.sleep(3000);
+    Zenhan.doTogame();
+  }
+}
