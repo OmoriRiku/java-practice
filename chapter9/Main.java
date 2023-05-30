d783629fbd9a087abf1555364ecdaaf29f773ae2
OmoriRiku <121606498+OmoriRiku@users.noreply.github.com>
2023-05-30 10:57:08 +0900

    スッキリわかるJava入門

--- chapter9/Main.java
+++ chapter9/Main.java
@@ -1,8 +1,8 @@
 public class Main {
   public static void main (String[] args) {
-    Hero h1 = new Hero();
-
-    System.out.println(h1.hp);
-    System.out.println(h1.name);
+    Thief t = new Thief("アサカ");
+    System.out.println(t.name);
+    System.out.println(t.hp);
+    System.out.println(t.mp);
   }
 }
