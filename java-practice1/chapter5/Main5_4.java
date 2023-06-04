4bcf372c06f6a0d415e894fca5774ded90af150f
OmoriRiku <121606498+OmoriRiku@users.noreply.github.com>
2023-06-04 22:32:10 +0900

    スッキリわかるJava入門

---
+++ java-practice1/chapter5/Main5_4.java
@@ -0,0 +1,10 @@
+public class Main5_4 {
+
+  public static void main (String[] args) {
+    hello("ミナト");
+  }
+
+  public static void hello(String name) {
+    System.out.println(name + "さん、こんにちは");
+  }
+}
