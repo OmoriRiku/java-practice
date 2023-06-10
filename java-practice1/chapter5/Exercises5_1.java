66df1b2baab90a6f7ddbd8f5fb08d106062ef0ad
OmoriRiku <oomori3321@gmail.com>
2023-06-10 04:14:21 +0000

    スッキリわかるJava入門

---
+++ java-practice1/chapter5/Exercises5_1.java
@@ -0,0 +1,17 @@
+public class Exercises5_1 {
+  public static void introduceOneselef() {
+    String name = "大森陸";
+    int age = 27;
+    double height = 165.6;
+    char zodiac = '猪';
+    
+    System.out.println("名前は" + name + "です。");
+    System.out.println("年齢は" + age + "です。");
+    System.out.println("身長は" + height + "です。");
+    System.out.println("干支は" + zodiac + "です。");
+  }
+  
+  public static void main(String[] args) {
+    introduceOneselef();
+  }
+}
