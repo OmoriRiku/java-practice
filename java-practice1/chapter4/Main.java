7025985b240c6c97c0a52503f12236c594915d40
OmoriRiku <oomori3321@gmail.com>
2023-06-14 15:12:02 +0000

    スッキリわかるJava入門 復習

---
+++ java-practice1/chapter4/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    int[] score = {50, 60, 65, 55, 70};
+    
+    for(int value : score) {
+      System.out.println(value);
+    }
+  }
+}
