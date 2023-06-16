8488bca77acaeda69813bce2e5192c5752a841fc
OmoriRiku <oomori3321@gmail.com>
2023-06-16 09:17:30 +0000

    スッキリわかるJava入門

---
+++ java-practice1/chapter4/Main4_9.java
@@ -0,0 +1,10 @@
+public class Main4_9 {
+  public static void main(String[] args) {
+    int[] scores = new int[] {20, 30, 40, 50, 80};
+    int sum = 0;
+    
+    for (int i = 0; i < scores.length; i++) {
+      System.out.println(scores[i]);
+    }
+  }
+}
