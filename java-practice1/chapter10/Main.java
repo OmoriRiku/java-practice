4cf2a47e31e05fa90d5a10bb778d50786ad9ec18
OmoriRiku <oomori3321@gmail.com>
2023-06-18 04:43:04 +0000

    スッキリわかるJava入門

---
+++ java-practice1/chapter10/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    SuperHero sh = new SuperHero();
+    Matango m = new Matango();
+    
+    sh.run();
+    
+    System.out.println(m.name);
+  }
+}
---
+++ java-practice1/chapter10/Matango.java
@@ -0,0 +1,9 @@
+public class Matango {
+  String name;
+  int hp = 100;
+  final int LEVEL = 10;
+  
+  public Matango() {
+    this.name = "お化けキノコ";
+  }
+}
