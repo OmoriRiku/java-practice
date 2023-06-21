186c9ebe653e78c80b506064ee5a54b3cccc7824
OmoriRiku <oomori3321@gmail.com>
2023-06-21 06:29:02 +0000

    javaの復習

--- java-practice1/Main.java
+++ java-practice1/Main.java
@@ -1,14 +1,11 @@
 public class Main {
   public static void main(String[] args) {
-    int[] scores = {30, 40, 50, 66, 40};
-    int count = 0;
-    
-    for (int value : scores) {
-      if (value >= 50) {
-        count++;
-      }
-    }
-    
-    System.out.println(count);
+    double calcArea = calcTriangleArea(10.0, 5.0);
+    System.out.println("三角形の面積：" + calcArea);
+  }
+  
+  public static double calcTriangleArea(double bottom, double height) {
+    double area = ((bottom * height) / 2);
+    return area;
   }
 }
