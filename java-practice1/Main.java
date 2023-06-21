0737cb34a93d8ec50e2bca3b61e10842ffef9985
OmoriRiku <oomori3321@gmail.com>
2023-06-21 06:06:27 +0000

    Java-practice

--- java-practice1/Main.java
+++ java-practice1/Main.java
@@ -1,8 +1,5 @@
 public class Main {
-  public static void hello(String name) {
-    System.out.println(name + "さん、こんにちは！");
-  }
   public static void main(String[] args) {
-    hello("ミナト");
+    System.out.println("Hello world!");
   }
 }
