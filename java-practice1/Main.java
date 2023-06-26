b4740aa503cac3e6db33d3f85cab365855c0bb01
OmoriRiku <oomori3321@gmail.com>
2023-06-26 04:45:38 +0000

    java

--- java-practice1/Main.java
+++ java-practice1/Main.java
@@ -1,11 +1,15 @@
 public class Main {
   public static void main(String[] args) {
-    double calcArea = calcTriangleArea(10.0, 5.0);
-    System.out.println("三角形の面積：" + calcArea);
+    self();
   }
   
-  public static double calcTriangleArea(double bottom, double height) {
-    double area = ((bottom * height) / 2);
-    return area;
+  public static void self() {
+    String name = "大森";
+    int age = 27;
+    double height = 165.5;
+    
+    System.out.println("名前は" + name + "です");
+    System.out.println("年齢は" + age + "です");
+    System.out.println("身長は" + height + "です");
   }
 }
