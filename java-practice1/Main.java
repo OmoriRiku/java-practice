05abbb2fa62c8c12ae79df919b08dfb1761076b9
OmoriRiku <oomori3321@gmail.com>
2023-06-29 05:47:04 +0000

    Hello world

--- java-practice1/Main.java
+++ java-practice1/Main.java
@@ -1,15 +1,5 @@
 public class Main {
   public static void main(String[] args) {
-    self();
-  }
-  
-  public static void self() {
-    String name = "大森";
-    int age = 27;
-    double height = 165.5;
-    
-    System.out.println("名前は" + name + "です");
-    System.out.println("年齢は" + age + "です");
-    System.out.println("身長は" + height + "です");
+    System.out.println("Hello world");
   }
 }
