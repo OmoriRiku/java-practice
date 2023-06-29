1be8ee509524d7036a193759f63604cedaf88e29
OmoriRiku <oomori3321@gmail.com>
2023-06-29 05:49:12 +0000

    配列の回し

--- java-practice1/Main.java
+++ java-practice1/Main.java
@@ -1,5 +1,14 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world");
+    int[] scores = new int[] {27, 37, 47, 50, 57};
+    int count = 0;
+    
+    for (int value : scores) {
+      if (value >= 50) {
+        count++;
+      }
+    }
+    
+    System.out.println("50点以上の科目：" + count);
   }
 }
