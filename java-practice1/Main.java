ab9a7b7b303667e6101d6d2c5776eb9fb73ddf38
OmoriRiku <oomori3321@gmail.com>
2023-07-01 08:47:42 +0000

    内定祝い

--- java-practice1/Main.java
+++ java-practice1/Main.java
@@ -1,31 +1,10 @@
 public class Main {
+  
   public static void main(String[] args) {
-    int season = new java.util.Scanner(System.in).nextInt();
-    
-    switch (season) {
-      case 3:
-      case 4:
-      case 5:
-        System.out.println("春です");
-        break;
-      case 6:
-      case 7:
-      case 8:
-        System.out.println("夏です。");
-        break;
-      case 9:
-      case 10:
-      case 11:
-        System.out.println("秋です。");
-        break;
-      case 12:
-      case 1:
-      case 2:
-        System.out.println("冬です。");
-        break;
-      default:
-        System.out.println("1~12の数値を入力してください");
-        break;
-    }
+    information("内定おめでとう。これから頑張っていこう");
   }
+  
+  public static void information(String text) {
+    System.out.println(text);
+  } 
 }
