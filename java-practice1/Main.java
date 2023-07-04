4ed20bfbd6988fc9240b56c4a8c826b8d3f41fd5
OmoriRiku <oomori3321@gmail.com>
2023-07-04 02:43:35 +0000

    java

--- java-practice1/Main.java
+++ java-practice1/Main.java
@@ -1,10 +1,11 @@
 public class Main {
-  
   public static void main(String[] args) {
-    information("内定おめでとう。これから頑張っていこう");
+    System.out.println("名前を入力してください");
+    String name = new java.util.Scanner(System.in).nextLine();
+    
+    System.out.println("年齢を入力してください");
+    int age = new java.util.Scanner(System.in).nextInt();
+    
+    System.out.println(age + "歳の" + name + "さん、こんにちは");
   }
-  
-  public static void information(String text) {
-    System.out.println(text);
-  } 
 }
