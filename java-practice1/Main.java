f873e6e5073b44213feb0194a698547d6456c219
OmoriRiku <oomori3321@gmail.com>
2023-07-04 02:50:14 +0000

    java

--- java-practice1/Main.java
+++ java-practice1/Main.java
@@ -1,11 +1,15 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("名前を入力してください");
-    String name = new java.util.Scanner(System.in).nextLine();
+    int[] scores = new int[5];
     
-    System.out.println("年齢を入力してください");
-    int age = new java.util.Scanner(System.in).nextInt();
+    System.out.println("数値を" + scores.length + "入力してください");
+    for(int i = 0; i < scores.length; i++) {
+      scores[i] = new java.util.Scanner(System.in).nextInt();
+    }
     
-    System.out.println(age + "歳の" + name + "さん、こんにちは");
+    System.out.println("出力します");
+    for (int i = 0; i < scores.length; i++) {
+      System.out.println(scores[i]);
+    }
   }
 }
