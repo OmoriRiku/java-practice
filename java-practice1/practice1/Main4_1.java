0db848cb7fddd2739949fed08e6b8326be674f74
OmoriRiku <oomori3321@gmail.com>
2023-07-08 05:45:03 +0000

    java

---
+++ java-practice1/practice1/Main4_1.java
@@ -0,0 +1,10 @@
+//  条件を満たす配列を準備。初期化なし。
+
+public class Main4_1 {
+  public static void main(String[] args) {
+    int[] points = new int[4];
+    double[] weights = new double[5];
+    boolean[] answers = new boolean[3];
+    String[] names = new String[3];
+  }
+}
